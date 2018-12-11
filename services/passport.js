45cc9466f8ad0b428d50d0a3008858111f9be0c7
Tolulope Odueke <tolulope.od@gmail.com>
2018-12-11 16:35:56 +0100

    Refactored promises to use ES2017 Async, Await syntax

--- services/passport.js
+++ services/passport.js
@@ -23,20 +23,18 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // We already have a record with the give profile ID
-          done(null, existingUser);
-        } else {
-          // Make a new user
-          new User({
-            googleId: profile.id
-          })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // We already have a record with the give profile ID
+        done(null, existingUser);
+      } else {
+        // Make a new user
+        const user = await new User({
+          googleId: profile.id
+        }).save();
+        done(null, user);
+      }
     }
   )
 );
