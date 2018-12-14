39bad25544fe92fe1c4ee0797318bb6f48eae976
Tolulope Odueke <tolulope.od@gmail.com>
2018-12-14 11:16:04 +0100

    Set up payments handling with react stripe checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Payments from "./Payments";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return;
+        return <li>Loading...</li>;
       case false:
         return (
           <li>
@@ -13,11 +15,14 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a href="api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
 
@@ -25,9 +30,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo" href="/#">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             MailFeed
-          </a>
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>MailFeed!</h1>
+      Collect feedback from users
+    </div>
+  );
+};
+
+export default Landing;
