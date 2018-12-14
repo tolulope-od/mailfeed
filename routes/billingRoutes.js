03e815fbacc9e43ace54a6a7caf71165f6c99f25
Tolulope Odueke <tolulope.od@gmail.com>
2018-12-14 20:05:05 +0100

    Added middleware for Login authentication

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,15 +1,12 @@
 // Bring in keys
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
   // @POST api/stripePayment
   // DESC add credits to account
-  app.post("/api/stripePayment", async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({ error: "You must log in!" });
-    }
-
+  app.post("/api/stripePayment", requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
